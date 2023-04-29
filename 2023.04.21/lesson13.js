6ac850dd2e6d53ffd549787bcc76f024935c9e4d
Kseniia <kvxoxlova2005@mail.ru>
2023-04-29 12:02:45 +0300

    5 sorts

--- 2023.04.21/lesson13.js
+++ 2023.04.21/lesson13.js
@@ -1,63 +1,107 @@
-// дописать пузырёк
-
-// function bubbleSort(array) {
-//     for (let i = 0; i < array.length - 1; i++){
-//         for (let j = 0; j < array.length -i-1; j++){
+// function bubbleSort(array){
+//     for (let i = 0; i < array.length; i++){
+//         for (let j = 0; j < array.length-i; j++){
 //             if (array[j] > array[j+1]){
-//                 array[j] = array[j+]
-//             }
-//         }
-//     }
-// }
-
-// доделать
-
-// function insertionSort(array){
-//     for (let i = 1; i < array.length; i++){
-//         let temp = array[i];
-//         let j = i-1;
-//         while(j >=0 && temp < array[j]){
-//             array[j+1] = array[j];
-//             j -= 1;
-//         }
-//         array[j+1] = temp;
-//         return array;
-//     }
-// }
-// let array = [32, 15, 204, 5];
-// console.log(insertionSort(array));
-
-
-// доделать
-
-// function selectionSort(array){
-//     for (let i in array){
-//         let min = i;
-//         for(let j = i+1; j < array.length-1; j++){
-//             if (array[j] < array[min]){
-//                 min = j;
-//             }
-//         }
-//         array[i] = array[min];
-//         array[min] = array[i];
-//         return array;
-//     }
-// }
-
-// let array = [32, 15, 204, 5];
-// console.log(selectionSort(array));
-
-
-// function shakerSort(array){
-//     for (let i = 1; i < array.length; i++){
-//         for (let j = 0; j < array.length - i; j++){
-//             if (array[j] > array[j+1]){
+//                 let temp = array[j];
 //                 array[j] = array[j+1];
+//                 array[j+1] = temp;
 //             }
 //         }
 //     }
 //     return array;
 // }
 
-// let array = [32, 15, 204, 5, 10, 3, 100, 33, 9];
+// let array = [12, 4, 41, 56, 8, 13, 90, 101, 32, 76, 3];
 // console.log(bubbleSort(array));
+
+
+// function insertionSort(array){
+//     for (let i = 1; i < array.length; i++){
+//         let temp = array[i];
+//         j = i-1;
+//         while (j>=0 && temp < array[j]){
+//             array[j+1] = array[j];
+//             j -= 1;
+//         }
+//         array[j+1] = temp;
+//     }
+//     return array;
+// }
+
+// let array = [12, 4, 41, 56, 8, 13, 90, 101, 32, 76, 3];
+// console.log(insertionSort(array));
+
+
+// function selectionSort(array){
+//     for (let i = 0; i < array.length; i++){
+//         let min = i;
+//         for (let j = i+1; j < array.length; j++){
+//             if (array[j] < array[min]){
+//                 min = j;
+//             }
+//         }
+//         let temp = array[i];
+//         array[i] = array[min];
+//         array[min] = temp;
+//     }
+//     return array;
+// }
+
+// let array = [12, 4, 41, 56, 8, 13, 90, 101, 32, 76, 3];
+// console.log(selectionSort(array));
+
+
+// function shakerSort(array){
+//     let changer = true;
+//     while (changer){
+//         for (let i = 0; i < array.length; i++){
+//             if (array[i] > array[i+1]){
+//                 let temp = array[i];
+//                 array[i] = array[i+1];
+//                 array[i+1] = temp;
+//                 changer = true;
+//             }
+//         }
+//         if (!changer)
+//         break;
+//         changer = false;
+//         for (let j = array.length - 1; j >= 0; j--){
+//             if (array[j] < array[j-1]){
+//                 let temp = array[j];
+//                 array[j] = array[j-1];
+//                 array[j-1] = temp;
+//                 changer = true;
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+// let array = [12, 4, 41, 56, 8, 13, 90, 101, 32, 76, 3];
+// console.log(shakerSort(array));
+
+
+// function shellSort(array){
+//     let gap = Math.floor(array.length / 2);
+//     while (gap > 0){
+//         for (let i = gap; i < array.length; i++){
+//             let j = i;
+//             let temp = array[i];
+//             while (j >= gap && array[j-gap] > temp) {
+//                 array[j] = array[j-gap];
+//                 j = j - gap;
+//             }
+//             array[j] = temp;
+//         }
+//         if (gap == 2) {
+//             gap = 1;
+//         } 
+//         else {
+//             gap = Math.floor(gap / 2);
+//         }
+//     }
+//     return array;
+// }
+
+// let array = [12, 4, 41, 56, 8, 13, 90, 101, 32, 76, 3];
+// console.log(shellSort(array));
